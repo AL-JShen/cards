435a6c14139c5cefb94336ae9617d14aa5960c01
Jeff Shen <jshen@protonmail.ch>
2017-03-31 21:17:01 -0700

    Completed project

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Icon } from 'react-fa';
 import './App.css'
 
 export default class Navbar extends Component {
@@ -6,14 +7,16 @@ export default class Navbar extends Component {
     return (
       <div className='navbar'>
         <div className='title'>
-          <h1>React Instagram</h1>
+          <span><Icon name='instagram' /></span><h1>React Instagram</h1>
+        </div>
+        <div className='search'>
+            <span><Icon name='search' /></span>
+            <span>Search</span>
         </div>
         <div className='nav'>
-          <ul>
-            <a href='#'>Home</a>
-            <a href='#'>Profile</a>
-            <a href='#'>About</a>
-          </ul>
+            <span><Icon name='compass' /></span>
+            <span><Icon name='user-o' /></span>
+            <span><Icon name='heart-o' /></span>
         </div>
       </div>
     );
